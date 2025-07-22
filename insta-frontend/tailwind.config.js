f76be3b8834020b6fb30ff8f66edf9e018eccb0b
suvetharani <suvetharanic@gmail.com>
2025-07-22 10:06:09 +0530

    modified the white/dark theme

--- insta-frontend/tailwind.config.js
+++ insta-frontend/tailwind.config.js
@@ -1,8 +1,9 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
